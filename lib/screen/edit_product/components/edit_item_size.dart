ac5e3029ffdeb23b04ae3c7095795c862f2c9b24
wilsonraj-dev <wilsonraj@unipam.edu.br>
2021-07-12 21:12:14 -0300

    Adicionando/removendo um tamanho

--- lib/screen/edit_product/components/edit_item_size.dart
+++ lib/screen/edit_product/components/edit_item_size.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:projeto_pi_flutter/common/custom_drawer/custom_icon_button.dart';
 import 'package:projeto_pi_flutter/model/item_size.dart';
 
 class EditItemSize extends StatelessWidget {
 
-  const EditItemSize({this.size});
+  const EditItemSize({this.size, this.onRemove});
   final ItemSize size;
+  final VoidCallback onRemove;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
         Expanded(
+          flex: 30,
           child: TextFormField(
             initialValue: size.name,
             decoration: const InputDecoration(
@@ -22,8 +25,9 @@ class EditItemSize extends StatelessWidget {
         ),
         const SizedBox(width: 4),
         Expanded(
+          flex: 30,
           child: TextFormField(
-            initialValue: size.stock.toString(),
+            initialValue: size.stock?.toString(),
             decoration: const InputDecoration(
               labelText: "Estoque",
               isDense: true,
@@ -33,14 +37,31 @@ class EditItemSize extends StatelessWidget {
         ),
         const SizedBox(width: 4),
         Expanded(
+          flex: 40,
           child: TextFormField(
-            initialValue: size.price.toStringAsFixed(2),
+            initialValue: size.price?.toStringAsFixed(2),
             decoration: InputDecoration(
               labelText: "Preço",
-              isDense: true
+              isDense: true,
+              prefixText: 'R\$',
             ),
             keyboardType: TextInputType.numberWithOptions(decimal: true),
           ),
+        ),
+
+        //
+        CustomIconButton(
+          iconData: Icons.remove,
+          color: Colors.red,
+          onTap: onRemove,
+        ),
+        CustomIconButton(
+          iconData: Icons.arrow_drop_up,
+          color: Colors.black,
+        ),
+        CustomIconButton(
+          iconData: Icons.arrow_drop_down,
+          color: Colors.black,
         )
       ],
     );
--- lib/screen/edit_product/components/sizes_form.dart
+++ lib/screen/edit_product/components/sizes_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_pi_flutter/common/custom_drawer/custom_icon_button.dart';
 import 'package:projeto_pi_flutter/model/item_size.dart';
 import 'package:projeto_pi_flutter/model/product.dart';
 
@@ -12,14 +13,43 @@ class SizesForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FormField<List<ItemSize>>(
-      initialValue: product.sizes,
+      initialValue: List.from(product.sizes),
       builder: (state) {
         return Column(
-          children: state.value.map((size){
-            return EditItemSize(
-              size: size,
-            );
-          }).toList(),
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Tamanhos',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                CustomIconButton(
+                  iconData: Icons.add,
+                  color: Colors.black,
+                  onTap: () {
+                    state.value.add(ItemSize());
+                    state.didChange(state.value);
+                  },
+                )
+              ],
+            ),
+            Column(
+              children: state.value.map((size){
+                return EditItemSize(
+                  size: size,
+                  onRemove: () {
+                    state.value.remove(size);
+                    state.didChange(state.value);
+                  },
+                );
+              }).toList(),
+            ),
+          ],
         );
       },
     );
